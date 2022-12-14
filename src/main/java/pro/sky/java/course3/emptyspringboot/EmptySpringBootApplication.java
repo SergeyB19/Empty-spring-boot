f0b40401db6544b987e337f45daf5b4073b39079
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-12-14 16:17:36 +0300

    empty-spring-boot

--- src/main/java/pro/sky/java/course3/emptyspringboot/EmptySpringBootApplication.java
+++ src/main/java/pro/sky/java/course3/emptyspringboot/EmptySpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmptySpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmptySpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmptySpringBootApplication.class, args);
+    }
 
 }
