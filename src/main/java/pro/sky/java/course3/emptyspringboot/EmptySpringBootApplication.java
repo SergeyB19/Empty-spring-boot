8084e0281a84a47332cd67046bd408a55829046d
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-12-14 16:11:54 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course3/emptyspringboot/EmptySpringBootApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.emptyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/java/course3/emptyspringboot/EmptySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.emptyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
